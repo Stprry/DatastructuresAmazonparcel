fe85e41ccaa3df537f24805f6533b2ef90b02b3e
Sam <stprry96@gmail.com>
2020-05-14 15:26:08 +0100

    FaketreeSort Insert Error

--- DatastructuresAmazonparcel/DatastructuresAmazonparcel/TreeSort.h
+++ DatastructuresAmazonparcel/DatastructuresAmazonparcel/TreeSort.h
@@ -13,7 +13,9 @@ class TreeSort
 public:
 	BoxSort bs;
 	vector<string> x = bs.sortBox();
-	vector<string, vector<string>> fakeTreeSort;
+	//vector<string,vector<string> > fakeTreeSort;//TODO Is this the issue?
+	vector<vector<string> > fakeTreeSort;
+		
 	vector<int>::iterator it;
 
 	void test()
@@ -25,28 +27,43 @@ public:
 			string firstChar = i->substr(0, 1);
 			std::cout << "Identified First Char" << firstChar << endl;
 
-			vector<string>::iterator it;
-			it = find(fakeTreeSort.begin(), fakeTreeSort.end(), firstChar);
-			if (it != fakeTreeSort.end())
+			//vector<string>::iterator it;
+			//it = find(fakeTreeSort.begin(), fakeTreeSort.end(), firstChar);
+			//if (it != fakeTreeSort.end())
+			//{
+			//	// element found so add to faketreesort
+			//	it = fakeTreeSort.insert(it, firstChar);
+			//}
+			//else
+			//{
+			//	//element not found so new element and reached end of vector
+			//	std::cout << "End of vector";
+			//}
+			bool isPresent = (find(cbegin(fakeTreeSort), cend(fakeTreeSort), firstChar) != cend(fakeTreeSort));
+			if (isPresent == true)
 			{
-				// element found so add to faketreesort
-				it = fakeTreeSort.insert(it, firstChar);
-			}
-			else
+				//isPresent = fakeTreeSort.insert(isPresent, firstChar);
+				fakeTreeSort.insert(fakeTreeSort.begin() + isPresent , firstChar);
+			}else
 			{
-				//element not found so new element and reached end of vector
-				std::cout << "End of vector something went wrong!";
+				// no element found and reached end of vector
+				std::cout << "End Of Vector Reached";
 			}
 		}
 
 		// print out
 		std::cout << "-------------------------------------------";
-
-		for (string firstChar : fakeTreeSort)
+		/*for (string firstChar : fakeTreeSort)
 		{
 			vector<string> enteries = fakeTreeSort[firstChar];
 			std::cout << "Character Key: " << firstChar;
 			for (auto first_char : enteries)(std::cout << "--------->" << first_char << endl);
+		}*/
+		
+		for (auto i = x.begin(); i != x.end();i++)
+		{
+			fakeTreeSort.pop_back();
 		}
+
 	}
 };
